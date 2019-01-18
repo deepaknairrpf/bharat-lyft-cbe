f5fd1bf0f413086da4e1ec3b90b70410f8e191b7
Deepak <deepaknairrpf@gmail.com>
2019-01-19 05:05:31 +0530

    Refactored user serializer

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        exclude = ('password', )
 
 
 class LyfteeScheduleSerializer(serializers.ModelSerializer):
