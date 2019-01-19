faf74c636b905a23bbb75a2bf52f725224dc2267
Sricharan <sricharan14312@cse.ssn.edu.in>
2019-01-19 11:19:51 +0530

    settings staging

--- lyft/settings_staging.py
+++ lyft/settings_staging.py
@@ -5,6 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'lyft_user',
         'USER': 'lyft_user',
+        'PASSWORD': 'lyft_user',
         'HOST': 'localhost',
         'PORT': 5432,
     }
